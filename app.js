7c483b187bff1e2a041fdbe53ff617ef61390217
Rob Miller <rob@bigfish.co.uk>
2018-02-06 16:10:57 +0000

    First pass at calculating violating years
    All 12-month periods are iterated over, and the overlapping portions of
    absences are found. The total number of days in each absence are
    calculated; if that total is greater than 180 days, the year in question
    is added to the "violating years" array, which is then output.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app = new Vue({
       end:   "2018-02-06",
     },
     years: [],
+    violating_years: [],
     absences: [],
     total_absent_days: 0
   },
@@ -24,7 +25,7 @@ var app = new Vue({
 
       while ( true ) {
         var end = moment(start).add(1, 'y').subtract(1, 'd');
-        this.years.push({ start: moment(start), end: end });
+        this.years.push({ start: moment(start), end: end, total_absences: 0 });
         start.add(1, 'd');
 
         if ( end >= period_end ) {
@@ -39,23 +40,52 @@ var app = new Vue({
       this.total_absent_days = 5;
     },
     calculate_periodic_absences: function() {
-      // For each 12 month period, fetch the absences in that period
-      // That means:
-      //    - the absences that overlap this period in any way;
-      //    - reduced to only the days that overlap.
-      // Then: sum the overlapping absent days to find a total for that
-      // period
-      // Move onto the next period
+      var self = this;
+      self.violating_years = [];
+      self.years.forEach(function(year) {
+        year.total_absences = 0;
+
+        var absences = self.absences.map(function(absence) {
+          if ( self.overlaps(absence, year) ) {
+            // Clamp the absence within this period
+            var start = Math.max(moment(year.start).format("X"), moment(absence.start).format("X"));
+            var end   = Math.min(moment(year.end).format("X"), moment(absence.end).format("X"));
+            // Calculate the number of days
+            return Math.ceil((moment(end, "X").add(23, "hours").format("X") - start) / 86400);
+          } else {
+            return 0;
+          }
+        });
+
+        year.total_absences = absences.reduce((a, c) => a + c);
+        if ( year.total_absences > 180 ) {
+          self.violating_years.push(year);
+        }
+      });
     },
     parse_absences: function() {
       var input = document.getElementById("absences").value;
       this.absences = input.split(/\n|\r/)
         .filter(l => l.match(/^\d{4}-\d{2}-\d{2}\s+(-|–)\s+\d{4}-\d{2}-\d{2}$/))
         .map(a => a.split(/\s+(?:-|–)\s+/))
-        .map(a => { return { start: a[0], end: a[1] }; });
+        .map(a => { return { start: moment(a[0]), end: moment(a[1]) }; });
+
+        this.calculate_periodic_absences();
+        this.calculate_total_absent_days();
+    },
+    overlaps: function(period_one, period_two) {
+      if ( period_one.start.isBetween(period_two.start, period_two.end, null, "[]") ) {
+        return true;
+      }
+
+      if ( period_one.end.isBetween(period_two.start, period_two.end, null, "[]") ) {
+        return true;
+      }
+
+      return false;
     },
   }
 });
 
-app.calculate_years();
 app.parse_absences();
+app.calculate_years();
