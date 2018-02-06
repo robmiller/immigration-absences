49db021d98ea60d7ce5dfd8ec0868cc1eb4e869f
Rob Miller <rob@bigfish.co.uk>
2018-02-06 13:47:06 +0000

    WIP: calculating periodic absences
    Just a step through the algorithm, no code yet

--- app.js
+++ app.js
@@ -28,11 +28,21 @@ var app = new Vue({
 
       this.period.end = moment(period_start).add(5, 'y').subtract(1, 'd').format("YYYY-MM-DD");
 
+      this.calculate_periodic_absences();
       this.calculate_total_absent_days();
     },
     calculate_total_absent_days: function() {
       this.total_absent_days = 5;
-    }
+    },
+    calculate_periodic_absences: function() {
+      // For each 12 month period, fetch the absences in that period
+      // That means:
+      //    - the absences that overlap this period in any way;
+      //    - reduced to only the days that overlap.
+      // Then: sum the overlapping absent days to find a total for that
+      // period
+      // Move onto the next period
+    },
   }
 });
 
